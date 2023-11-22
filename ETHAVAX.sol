724ec799471278ef1aae136be950c5f3cbe21895
Anandu Singh <146640432+anandusingh@users.noreply.github.com>
2023-11-22 11:57:53 +0530

    Update ETHAVAX.sol

--- ETHAVAX.sol
+++ ETHAVAX.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract anandu {
+    address public owner;
+    uint256 public number = 5;
+
+    event numberUpdated(uint256 newnumber);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function updatenumber(uint256 newnumber) external {
+        // Use require() to validate input
+        require(newnumber > 0, "number must be greater than zero");
+ 
+        // Use assert() to check for invariants
+        assert(msg.sender == owner);
+
+        // Use revert() to provide custom error message
+        if (newnumber > 10) {
+            revert("number cannot exceed 10");
+        }
+
+        number = newnumber;
+        emit numberUpdated(newnumber);
+    }
+}
+
 
